309fc939ea9131661407ada88fdc642ad9d5555b
Gauthier <gauthier.aze.2@gmail.com>
2024-01-30 15:09:32 +0100

    ee

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('./app.css') }}">
+
+</head>
+
+<body>
+
+
+    
+    <header >
+        
+        <button>ACCUEIL</button>
+        <button>DESTINATION</button>
+        <button>EQUIPAGE</button>
+        <button>TECHNOLOGIE</button>
+    
+        
+    </header>
+
+
+    <div class="conteneurImage">
+    
+    <div class="carreFlou">
+        <img src="{{asset('./Background.jpg')}}" alt="">
+    </div>
+
+    <div class="espace">
+        <div class="basgauche">
+           
+                <p>donc vous voulez voyagez dans l'espace</p>
+                <h1>L'ESPACE</h1>
+                <h2 >Soyons objectif; si vous voulez aller dans l'espace, vous pouvez aller véritablement <br>
+                    le véritable espace et non juste planer sur le bord de celui-ci. Eh bien,<br>
+                    asseyez-vous parce que nous allons vous donner une expérience vraiment extraordinaire!</h2>
+
+           
+        </div>
+        <div>
+            <p class="boutonRond"> bouton rond</p>
+       </div>
+    </div>
+</body>
+
+</html>
